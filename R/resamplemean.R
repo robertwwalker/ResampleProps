11b847a949d1b311889b27f816aa71271433432e
robertwwalker <rwalker@willamette.edu>
2018-10-18 19:27:43 -0700

    First Complete Commit

--- R/resamplemean.R
+++ R/resamplemean.R
@@ -1,6 +1,6 @@
 ResampleMean <- function(vec.data, k=1000) {
   ifelse((is.vector(vec.data) & is.numeric(vec.data))  ,{
-    vec.data <- vec.data[complete.cases(vec.data)]
+    vec.data <- vec.data[stats::complete.cases(vec.data)]
     Means <- sapply(1:k, function(x) { mean(vec.data[sample(1:length(vec.data), size=length(vec.data), replace=TRUE)])})
   }, stop(paste("Requires a numeric vector, not a",mode(vec.data), sep=" ")))
   return(Means)
